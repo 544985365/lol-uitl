73cd730d6fa2b18d36c467b4db5294464a8ca40b
544985365@qq.com <123456789qq>
2022-07-25 20:38:05 +0800

    Modifying the packaging Configuration

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,28 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  pluginOptions: {
-    electronBuilder: {
-        builderOptions: {
-            asar: false
-        },
-        preload: 'src/preload.js',
-        logo: 'src/assets/sullly.jpg'
+    transpileDependencies: true,
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                asar: false,
+                productName: 'LOL工具',
+                copyright: '2022',
+                compression: 'store',
+                nsis: {
+                    oneClick: false,
+                    allowToChangeInstallationDirectory: true,
+                    allowElevation: true,
+                    installerIcon: './public/icon.ico',
+                    installerHeaderIcon: './public/icon.ico'
+                },
+                appId: "top.biangege",
+                win: {
+                    target: "nsis",
+                    icon: "./public/icon.ico"
+                }
+            },
+            preload: 'src/preload.js',
+        }
     }
-}
 
 })
